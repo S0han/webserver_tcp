6588cac7d6b611d281aad088016fbec8f6d712fe
S0han <kane.pereira18@gmail.com>
2024-11-21 01:56:55 -0500

    update: add updates to accept and parse data + log statements

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ PORT = 80
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen(5)
+
+while True:
+    (client_socket, address) = server_socket.accept()
+    print(f"This is the address: {address}")
+    while True:
+        #the 1024 is the amount of bytes that will be read AT MOST
+        #will block if no data recieved
+        data = client_socket.recv(1024)
+        if not data: 
+            break
+        client_socket.sendall(data)
+        print(f"Currently recieving data: {data}")
+
+#close connection
+server_socket.close()
