7fbcb978c001a839fb8120da8c4c21bcc51b4fd7
S0han <kane.pereira18@gmail.com>
2024-11-21 02:19:55 -0500

    update: add close() command to client side to close connection once done

--- client.py
+++ client.py
@@ -2,12 +2,17 @@
 
 import socket
 
+#connect the client to the host and port of the server
 HOST = '127.0.0.1'
 PORT = 80
 
+#create the client socket that will send data to server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
-client_socket.sendall(b"TheKaneologist says Hi")
-data = client_socket(1024)
+client_socket.sendall(b"TheKaneologist says Hi!")
+data = client_socket.recv(1024)
 
 print(f"Server response received: {data}")
+
+#close connection
+client_socket.close()
