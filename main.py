03d93f35b0c47e49d23dd9967bfc639a8b9e1991
S0han <kane.pereira18@gmail.com>
2024-11-21 00:52:21 -0500

    update: abstract the arguemnts in the bind call

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import socket
 
+HOST = '127.0.0.1'
+PORT = 80
+
+#create the socket --> bind to localhost --> listen on port 80
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('127.0.0.1', 80))
+server_socket.bind((HOST, PORT))
 server_socket.listen(5)
